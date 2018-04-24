954f7acc0345783c742edfff0235ec14cadecad4
Jaap Jansma <jaap@edeveloper.nl>
2018-04-24 16:20:53 +0200

    fixes after review

--- CMRF_Drupal/SQLPersistingCallFactory.php
+++ CMRF_Drupal/SQLPersistingCallFactory.php
@@ -9,17 +9,7 @@
 
 namespace CMRF\Drupal;
 
-use mysqli;
-
 class SQLPersistingCallFactory extends \CMRF\PersistenceLayer\SQLPersistingCallFactory {
-  
-  /** @var mysqli */
-  private $connection;
-
-  public function __construct(mysqli $sql_connection, $table_name, callable $constructor, callable $loader) {
-    parent::__construct($sql_connection, $table_name, $constructor, $loader);
-    $this->connection=$sql_connection;
-  }
   
   public function clearCachedCalls() {
     $stmt = $this->connection->query("delete from {$this->table_name} where status = 'DONE'");
